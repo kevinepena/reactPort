1ad9502a37c1d11080b328baa8a2f7605ce15465
Kevin Pena <kevinpena160@gmail.com>
2018-08-10 16:16:41 -0500

    gallery descriptions

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -6,19 +6,40 @@ class Gallery extends Component {
 
     render() {
 
-        const works = [{ img: "assets/pics/work/work1.png", link: "https://freenica.herokuapp.com" }, { img: "assets/pics/work/work2.png", link: "https://auth1-81f49.firebaseapp.com/" }, { img: "assets/pics/work/work3.png", link: "https://kevinepena.github.io/wordGuessGame/" }, { img: "assets/pics/work/work4.png", link: "" }, { img: "assets/pics/work/work5.png", link: "" }]
+        const works = [{ img: "assets/pics/work/work1.png", link: "https://freenica.herokuapp.com", tech: ["Node", "Express", "React", "MongoDB", "Reactstrap", "Firebase", "Auth0"], about: "App for displaying trending information", list: ["Intergrated Auth0 with different level scopes", "Implemented Twitter API and socket.io to stream live tweets", "Full CRUD capabilities with MongoDB", "Uses Cloudinary API to host pictures", "Integrated Firebase database for extra secure storage", "Mobile Responsive Layout"] }, { img: "assets/pics/work/work2.png", link: "https://auth1-81f49.firebaseapp.com/", tech: ["Firebase", "Javascript", "jQuery"], about: "Platform for viewing astrology", list: ["Uses firebase authentication", "Integrated astrology and NASA API to provide user with astrology information"] }, { img: "assets/pics/work/work3.png", link: "https://kevinepena.github.io/wordGuessGame/", tech: ["Javascript", "CSS", "jQuery"], about: "Game to test Javascript code efficiency", list: [] },
+            // { img: "assets/pics/work/work4.png", link: "", tech: [], about: "" }, { img: "assets/pics/work/work5.png", link: [], tech: [], about: "" }
+        ]
 
         return (
             <div className={this.props.open ? "fixed blur" : "fixed"} style={{ height: ((this.props.winWidth > 768) ? "100vh" : "") }}>
 
                 <h1 id="list-item-3" style={{ fontFamily: 'Raleway', }}> Gallery </h1>
-                <div className="gallery" style={{ width: ((this.props.winWidth > 768) ? "2200px" : ""), marginBottom: "50px" }}>
+                <div className="gallery" style={{ width: ((this.props.winWidth > 768) ? "1400px" : ""), marginBottom: "50px" }}>
 
                     {works.map((image, index) => (
                         <div key={index} className="frame" style={{ display: ((this.props.winWidth > 768) ? "inline" : ""), margin: ((this.props.winWidth > 768) ? "40px" : "40px auto"), }} >
                             <a href={image.link} target="_blank">
+                                {/* <span id="imgtxt"> */}
+
+                                {/* </span> */}
                                 <img className="parallax" src={image.img} alt="" />
+                                {/* <div class="overlay"> */}
+                                <span className="content">
+                                    <h4>{image.link.slice(8)} </h4>
+                                    <h5> Technologies Used </h5>
+                                    {console.log(image.tech)}
+                                    <p className="tech"> {image.tech.map(item => ` ${item} |`)}
+                                    </p>
+                                    <p className="about">
+                                        {image.about}
+                                    </p>
+                                    <ul className="list">
+                                        {image.list.map(item => <li> {item} </li>)}
+                                    </ul>
+                                </span>
+                                {/* </div> */}
                             </a>
+
                         </div>
                     ))}
 
